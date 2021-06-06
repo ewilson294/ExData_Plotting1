a27f1c8759c4ba5acbcc4e53c68d44872751c8e9
ewilson294 <ecwil2@yahoo.com>
2021-06-05 20:10:19 -0500

    Subset dataframe to dates of interest
    Former-commit-id: b7058a5c4515dae2fe1e8cae24ff2ae4027cb0a1

--- plot1.R
+++ plot1.R
@@ -8,6 +8,6 @@ householdPowerConsumption[,3:9] <- sapply(householdPowerConsumption[,3:9], as.nu
 # Convert date characters to date objects
 householdPowerConsumption$Date <- as.Date(householdPowerConsumption$Date, format = "%d/%m/%Y")
 # Subset dataframe by date of interest
-
+householdPowerConsumption <- householdPowerConsumption[c(which(householdPowerConsumption$Date == "2007-02-01"), which(householdPowerConsumption$Date == "2007-02-02")),]
 # Construct histogram
 hist(householdPowerConsumption$Global_active_power) # 
