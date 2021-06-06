a6e867be2153aadcea5ccf8b060b102b356b0957
ewilson294 <ecwil2@yahoo.com>
2021-06-05 19:54:10 -0500

    create Histogram
    Former-commit-id: 4fa2ee45a026f8b50192fd83f21756c431d2671c

--- plot1.R
+++ plot1.R
@@ -3,3 +3,5 @@
 # Date Created: 5 June 2021
 
 householdPowerConsumption <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+householdPowerConsumption[,3:9] <- sapply(householdPowerConsumption[,3:9], as.numeric)
+hist(householdPowerConsumption$Global_active_power)
