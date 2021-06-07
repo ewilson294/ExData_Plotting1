37d8e3125ed1ffada32f2acdaa8aa3897995917a
ewilson294 <ecwil2@yahoo.com>
2021-06-06 21:59:51 -0500

    Convert date characters to date objects Subset Dataframe by date of interest (Bugged) Convert time to date objects

--- plot2.R
+++ plot2.R
@@ -1,3 +1,16 @@
-# plot1.R
+# plot2.R
 # Author: Eric Wilson
 # Date Created: 6 June 2021
+
+householdPowerConsumption <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
+# Convert characters in table to numeric values
+householdPowerConsumption[,3:9] <- sapply(householdPowerConsumption[,3:9], as.numeric)
+# Convert date characters to date objects
+householdPowerConsumption$Date <- as.Date(householdPowerConsumption$Date, format = "%d/%m/%Y")
+# Subset dataframe by date of interest
+householdPowerConsumption <- householdPowerConsumption[c(which(householdPowerConsumption$Date == "2007-02-01"), which(householdPowerConsumption$Date == "2007-02-02")),]
+# Convert time to date objects
+for (tau in 1:length(householdPowerConsumption$Time)) {
+    time <- paste(strftime(householdPowerConsumption$Date[tau]), householdPowerConsumption$Time[tau], sep = " ")
+    householdPowerConsumption$Time[tau] <- strptime(time, format = "%Y-%m-%d %R:%S")
+}
